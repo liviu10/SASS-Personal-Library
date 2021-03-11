c669df30f4da4bfb824fdea0e0304920536c5fb3
Liviu Voica <voica.liviu10@gmail.com>
2021-03-11 16:33:25 +0200

    Created the scroll-top SASS mixin component and added the javascript functionality

---
+++ utilities/javascript/scroll-top.js
@@ -0,0 +1,13 @@
+var scroll_top = document.getElementById("scroll_top");
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scroll_top.style.display = "block";
+    } else {
+        scroll_top.style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
